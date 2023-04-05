2c4f38fda0603ee6014075d7d1e0962c278a5e98
Sasha <117905137+Ghostinchik@users.noreply.github.com>
2023-04-05 18:25:17 +0300

    Layout designer

---
+++ Компонувальник/Компонувальник/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Компонувальник
+{
+    public partial class Form1 : Form
+    {
+
+        private LayoutDesigner.BicycleBuilder builder;
+
+        private LayoutDesigner.BicycleComponent component;
+        public Form1()
+        {
+            InitializeComponent();
+
+            builder = new LayoutDesigner.BicycleBuilder();
+            LayoutDesigner.BicycleComponent component = new LayoutDesigner.BicycleComponent("MyComponent", 10.0);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Збір компонентів велосипеду
+            double framePrice = Convert.ToDouble(textBox1.Text);
+            builder.BuildFrame(textBox1.Text, framePrice);
+
+            double wheelPrice = Convert.ToDouble(textBox2.Text);
+            builder.BuildWheel(textBox2.Text, wheelPrice);
+
+            double seatPrice = Convert.ToDouble(textBox3.Text);
+            builder.BuildSeat(textBox3.Text, seatPrice);
+
+            double handlebarPrice = Convert.ToDouble(textBox4.Text);
+            builder.BuildHandlebar(textBox4.Text, handlebarPrice);
+
+            // Додавання декораторів
+
+            if (checkBox1.Checked)
+            {
+                double wheelDecoratorPrice = Convert.ToDouble(textBox5.Text);
+                builder.AddWheelDecorator(textBox5.Text, wheelDecoratorPrice);
+            }
+
+            if (checkBox2.Checked)
+            {
+                double seatDecoratorPrice = Convert.ToDouble(textBox6.Text);
+                builder.AddSeatDecorator(textBox6.Text, seatDecoratorPrice);
+            }
+
+            if (checkBox3.Checked)
+            {
+                double handlebarDecoratorPrice = Convert.ToDouble(textBox7.Text);
+                builder.AddHandlebarDecorator(textBox7.Text, handlebarDecoratorPrice);
+            }
+
+             label1.Text = textBox8.Text;
+            
+           
+        
+           double sum = Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text)  + Convert.ToDouble(textBox3.Text) + Convert.ToDouble (textBox4.Text);
+            if (checkBox1.Checked)
+            {
+             sum = sum + Convert.ToDouble(textBox5.Text);
+            }
+            if (checkBox2.Checked)
+            {
+                sum = sum + Convert.ToDouble(textBox6.Text);
+            }
+            if (checkBox3.Checked)
+            {
+                sum = sum + Convert.ToDouble(textBox7.Text);
+            }
+            label2.Text = sum.ToString(); 
+        }
+    }
+}
+
---
+++ Компонувальник/Компонувальник/LayoutDesigner.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компонувальник
+{
+    internal class LayoutDesigner
+    {
+        public interface IBicycleComponent
+        {
+            string GetName();
+            double GetPrice();
+        }
+
+        // Простий компонент велосипеду
+        public class BicycleComponent : IBicycleComponent
+        {
+            private string name;
+            private double price;
+
+            public BicycleComponent(string name, double price)
+            {
+                this.name = name;
+                this.price = price;
+            }
+
+            public string GetName()
+            {
+                name = " Велосипед ";
+                return this.name;
+            }
+
+            public double GetPrice()
+            {
+                return this.price;
+            }
+        }
+
+        // Композитний компонент велосипеду
+        public class Composite : IBicycleComponent
+        {
+            private List<IBicycleComponent> components;
+
+            public Composite()
+            {
+                components = new List<IBicycleComponent>();
+            }
+
+            public void AddComponent(IBicycleComponent component)
+            {
+                components.Add(component);
+            }
+
+            public void RemoveComponent(IBicycleComponent component)
+            {
+                components.Remove(component);
+            }
+
+            public IBicycleComponent FindComponent(string name)
+            {
+                foreach (IBicycleComponent component in components)
+                {
+                    if (component.GetName() == name)
+                    {
+                        return component;
+                    }
+                }
+
+                return null;
+            }
+
+            public string GetName()
+            {
+                return "Composite";
+            }
+
+            public double GetPrice()
+            {
+                double price = 0;
+
+                foreach (IBicycleComponent component in components)
+                {
+                    price += component.GetPrice();
+                }
+
+                return price;
+            }
+        }
+
+        // Декоратор компонентів велосипеду
+        public abstract class BicycleDecorator : IBicycleComponent
+        {
+            private IBicycleComponent component;
+
+            public BicycleDecorator(IBicycleComponent component)
+            {
+                this.component = component;
+            }
+
+            public virtual string GetName()
+            {
+                return component.GetName();
+            }
+
+            public virtual double GetPrice()
+            {
+                return component.GetPrice();
+            }
+        }
+
+        // Декоратор колес велосипеду
+        public class WheelDecorator : BicycleDecorator
+        {
+            private string name;
+            private double price;
+
+            public WheelDecorator(IBicycleComponent component, string name, double price) : base(component)
+            {
+                this.name = name;
+                this.price = price;
+            }
+
+            public override string GetName()
+            {
+                return base.GetName() + " with " + name + " wheels";
+            }
+
+            public override double GetPrice()
+            {
+                return base.GetPrice() + price;
+            }
+        }
+
+        // Декоратор сидіння велосипеду
+        public class SeatDecorator : BicycleDecorator
+        {
+            private string name;
+            private double price;
+
+            public SeatDecorator(IBicycleComponent component, string name, double price) : base(component)
+            {
+                this.name = name;
+                this.price = price;
+            }
+
+            public override string GetName()
+            {
+                return base.GetName() + " with " + name + " seat";
+            }
+
+            public override double GetPrice()
+            {
+                return base.GetPrice() + price;
+            }
+        }
+
+        //Декоратор керма велосипеда
+        public class HandlebarDecorator : BicycleDecorator
+        {
+            private string name;
+            private double price;
+            public HandlebarDecorator(IBicycleComponent component, string name, double price) : base(component)
+            {
+                this.name = name;
+                this.price = price;
+            }
+
+            public override string GetName()
+            {
+                return base.GetName() + " with " + name + " handlebars";
+            }
+
+            public override double GetPrice()
+            {
+                return base.GetPrice() + price;
+            }
+        }
+        // Формування складових велосипеду
+        public class BicycleBuilder
+        {
+            private Composite bicycle;
+            public BicycleBuilder()
+            {
+                bicycle = new Composite();
+            }
+
+            public void BuildFrame(string frameName, double framePrice)
+            {
+                BicycleComponent frame = new BicycleComponent(frameName, framePrice);
+                bicycle.AddComponent(frame);
+            }
+
+            public void BuildWheel(string wheelName, double wheelPrice)
+            {
+                BicycleComponent wheel = new BicycleComponent(wheelName, wheelPrice);
+                bicycle.AddComponent(wheel);
+            }
+
+            public void BuildSeat(string seatName, double seatPrice)
+            {
+                BicycleComponent seat = new BicycleComponent(seatName, seatPrice);
+                bicycle.AddComponent(seat);
+            }
+
+            public void BuildHandlebar(string handlebarName, double handlebarPrice)
+            {
+                BicycleComponent handlebar = new BicycleComponent(handlebarName, handlebarPrice);
+                bicycle.AddComponent(handlebar);
+            }
+
+            public void AddWheelDecorator(string name, double price)
+            {
+                IBicycleComponent component = bicycle.FindComponent("Wheel");
+
+                if (component != null)
+                {
+                    WheelDecorator wheelDecorator = new WheelDecorator(component, name, price);
+                    bicycle.RemoveComponent(component);
+                    bicycle.AddComponent(wheelDecorator);
+                }
+            }
+
+            public void AddSeatDecorator(string name, double price)
+            {
+                IBicycleComponent component = bicycle.FindComponent("Seat");
+
+                if (component != null)
+                {
+                    SeatDecorator seatDecorator = new SeatDecorator(component, name, price);
+                    bicycle.RemoveComponent(component);
+                    bicycle.AddComponent(seatDecorator);
+                }
+            }
+
+            public void AddHandlebarDecorator(string name, double price)
+            {
+                IBicycleComponent component = bicycle.FindComponent("Handlebar");
+
+                if (component != null)
+                {
+                    HandlebarDecorator handlebarDecorator = new HandlebarDecorator(component, name, price);
+                    bicycle.RemoveComponent(component);
+                    bicycle.AddComponent(handlebarDecorator);
+                }
+            }
+            public Composite GetBicycle()
+            {
+                return bicycle;
+            }
+        }
+    }
+}
